95a9029145c60b4e63b60a1950af0e3329187d40
Ackshay <ackshayjain@gmail.com>
2018-05-24 00:08:48 +0530

    first

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from . import views
+
+
+
+urlpatterns = [
+    url(r'^$',views.index,name='index'),
+    
+    url(r'^login/$',views.login_view,name='login'),
+    url(r'^register/$',views.register_view,name='register'),
+    url(r'^logout/$',views.logout_view,name='logout'),
+]
